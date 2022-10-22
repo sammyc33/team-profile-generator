756b48320dcf310403e37d433684e838e57f0770
Sammy <sammy.campbell33@gmail.com>
2022-10-22 13:54:35 -0700

    update file tree according to BCS specs

--- index.js
+++ index.js
@@ -1,2 +1,30 @@
 const inquirer = require('inquirer');
 const fs = require('fs');
+
+const managerPrompts = [
+    {
+        type: "input",
+        message: "What is the title of your project?",
+        name: "title"
+    },
+    {
+        type: "checkbox",
+        message: "Select a license for your project:",
+        choices: ['MIT', 'Apache', 'BSD', 'None'],
+        name: "license",
+    },
+];
+
+const employeePrompts = [
+    {
+        type: "input",
+        message: "What is the title of your project?",
+        name: "title"
+    },
+    {
+        type: "checkbox",
+        message: "Select a license for your project:",
+        choices: ['MIT', 'Apache', 'BSD', 'None'],
+        name: "license",
+    },
+];
